88dcf1855d0f3b2b3f8f0991f0a7bf1857118b94
EtherSol <kylemz@email.arizona.edu>
2023-05-23 15:47:44 -0700

    First Test

---
+++ Azure/CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.UITests{
+
+    public class CreditCardWebAppShould
+    {   
+        [Fact]
+        [Trait("Category","Smoke")]
+        public void LoadApplicationPage() {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                
+            }
+        }
+    }
+};
