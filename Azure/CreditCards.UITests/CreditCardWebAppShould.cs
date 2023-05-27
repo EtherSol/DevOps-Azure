ea339d15a4ffd0407ce45d1808589002a3f23531
EtherSol <kylemz@email.arizona.edu>
2023-05-27 15:10:13 -0700

    Got Test Explorer Running on VSc

--- Azure/CreditCards.UITests/CreditCardWebAppShould.cs
+++ Azure/CreditCards.UITests/CreditCardWebAppShould.cs
@@ -3,16 +3,18 @@ using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-namespace CreditCards.UITests{
+namespace CreditCards.UITests
+{
 
     public class CreditCardWebAppShould
-    {   
+    {
         [Fact]
-        [Trait("Category","Smoke")]
-        public void LoadApplicationPage() {
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
             using (IWebDriver driver = new ChromeDriver())
             {
-                
+                driver.Navigate().GoToUrl("http://localhost:44108");
             }
         }
     }
